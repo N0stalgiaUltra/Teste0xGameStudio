929e51fccdd0fc3f41cce0dbcdcc1abce8097972
Vinicius Santos <viniciusantos0898@gmail.com>
2022-09-06 15:07:16 -0300

    Added the player rotation with mouse and key

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [Header ("Player Movement Variables")]
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed;
+
+    [Header("Bengal Variables")]
+    [SerializeField] private Transform bengalTransform;
 
     private float movX, movY = 0;
     void Start()
@@ -20,23 +23,56 @@ public class PlayerMovement : MonoBehaviour
     {
         movX = Input.GetAxis("Horizontal");
         movY = Input.GetAxis("Vertical");
-        Rotate();
 
     }
     private void FixedUpdate()
     {
         Move();
-
+        RotateBengal();
     }
     private void Move()
     {
         rb.velocity = new Vector2(movX * speed, movY * speed);
-    }
-    private void Rotate()
-    {
+
         if (movX > 0)
-            transform.rotation = Quaternion.Euler(Vector3.zero);
+            Rotate(true);
         else if (movX < 0)
+            Rotate(false);
+    }
+    private void Rotate(bool turn)
+    {
+        if(turn)
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+        else
+        {
             transform.rotation = Quaternion.Euler(0, 180, 0);
+               
+        }
+    }
+
+    private void RotateBengal()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 targetPos = Camera.main.WorldToScreenPoint(transform.localPosition);
+        Vector2 diff = new Vector2(mousePos.x - targetPos.x, mousePos.y - targetPos.y);
+        float angle = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+
+        if (angle < -90 || angle > 90)
+        {
+            if (this.transform.eulerAngles.y.Equals(0))
+            {
+                Rotate(false);
+                bengalTransform.localRotation = Quaternion.Euler(180, 0, -angle);
+            }
+           else if(this.transform.eulerAngles.y.Equals(180))
+                bengalTransform.localRotation = Quaternion.Euler(180, 180, -angle);
+
+
+        }
+        else
+        {
+            Rotate(true);
+            bengalTransform.localRotation = Quaternion.Euler(0f, 0f, angle);
+        }
     }
 }
