4a65cde32f74465ad4ca67af6eab44bc3eb5ec58
Vinicius Santos <viniciusantos0898@gmail.com>
2022-09-06 22:40:27 -0300

    My return from appointment; added enemy movement

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform playerTransform;
+    [SerializeField] private float speed;
     void Start()
     {
-        
+        playerTransform = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(playerTransform != null)
+        {
+            if (playerTransform.transform.position.x > this.transform.position.x)
+                this.transform.rotation = Quaternion.Euler(0, 180, 0);
+            else if (playerTransform.transform.position.x < this.transform.position.x)
+                this.transform.rotation = Quaternion.Euler(Vector3.zero);
+
+            this.transform.position = Vector2.MoveTowards(this.transform.position, playerTransform.position, speed * Time.deltaTime);
+        }
+
     }
 }
